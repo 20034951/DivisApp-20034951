3c43b7241adb8a3ba636aa52f82dfa867ff7f1e2
pablinchapin <pablinchapin@users.noreply.github.com>
2025-11-24 18:02:50 -0600

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,82 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    FormsModule,
+    CommonModule
+  ],
 })
 export class HomePage {
-  constructor() {}
+
+  tasaUsd: number | null = null;
+  cargando = false;
+  error: string | null = null;
+
+  quetzales: number | null = null;
+  resultado: number | null = null;
+
+  readonly DEFAULT_TASA_USD = 0.128;
+
+  constructor() {
+    this.obtenerTipoCambio();
+  }
+
+  async obtenerTipoCambio() {
+    this.cargando = true;
+    this.error = null;
+
+    try {
+      const res = await fetch("https://open.er-api.com/v6/latest/GTQ");
+      const data = await res.json();
+
+      this.tasaUsd = data.rates.USD; // 1 GTQ → USD
+    } catch (err) {
+        console.error("API de tipo de cambio falló:", err);
+
+        // Aplicar fallback automático
+        this.tasaUsd = this.DEFAULT_TASA_USD;
+
+        // Puedes mostrar un mensaje opcional
+        //this.error = "Usando tasa predeterminada por fallo del servicio.";
+    }
+
+    this.cargando = false;
+  }
+
+  convertir() {
+    if (this.tasaUsd && this.quetzales != null) {
+      this.resultado = this.quetzales * this.tasaUsd;
+    }
+  }
 }
